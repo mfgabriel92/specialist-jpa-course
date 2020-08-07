c88682bbd264d458206f6e4298eba3e605efc833
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-08-06 21:03:12 -0300

    2.9. Inserting the first object with the method persist

--- src/test/java/br/gabriel/jpaspecialist/initiatingwithjpa/OperationsWithTransactionTest.java
+++ src/test/java/br/gabriel/jpaspecialist/initiatingwithjpa/OperationsWithTransactionTest.java
@@ -1,17 +1,27 @@
 package br.gabriel.jpaspecialist.initiatingwithjpa;
 
 import br.gabriel.jpaspecialist.BaseEntityManager;
+import br.gabriel.jpaspecialist.model.Product;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class OperationsWithTransactionTest extends BaseEntityManager {
     @Test
-    public void openAndCloseTransaction() {
-//        Product product = new Product();
-        
+    public void shouldInsertNewProduct() {
+        Product product = new Product();
+        product.setId(2);
+        product.setName("Apple Macbook Pro");
+        product.setDescription("The most powerful notebooks featuring fast processors, incredible graphics, Touch Bar, and a spectacular Retina display");
+        product.setPrice(new BigDecimal("2399.00"));
+    
         entityManager.getTransaction().begin();
-//        entityManager.persist(product);
-//        entityManager.merge(product);
-//        entityManager.remove(product);
+        entityManager.persist(product);
         entityManager.getTransaction().commit();
+        entityManager.clear();
+        
+        Product assertProduct = entityManager.find(Product.class, product.getId());
+        Assert.assertNotNull(assertProduct);
     }
 }
