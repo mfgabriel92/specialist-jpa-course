9df0df59eba0c1028261dc99a4149676995711f6
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-08-16 12:01:40 -0300

    7.2. Configuring details of the table with @Table

--- src/main/java/br/gabriel/jpaspecialist/model/Client.java
+++ src/main/java/br/gabriel/jpaspecialist/model/Client.java
@@ -10,12 +10,18 @@ import java.util.List;
 import java.util.Map;
 
 @Entity
-@Table(name = "clients")
+@Table(
+    name = "clients",
+    uniqueConstraints = { @UniqueConstraint(name = "uk_clients_cpf", columnNames = { "cpf" }) },
+    indexes = { @Index(name = "idx_name", columnList = "name") }
+)
 @SecondaryTable(name = "clients_details", pkJoinColumns = @PrimaryKeyJoinColumn(name = "client_id"))
 @Setter
 @Getter
 public class Client extends BaseEntity {
     private String name;
+    
+    private String cpf;
     
     @Transient
     private String firstName;
@@ -28,10 +34,7 @@ public class Client extends BaseEntity {
     private LocalDate birthdate;
     
     @ElementCollection
-    @CollectionTable(
-        name = "clients_contacts",
-        joinColumns = @JoinColumn(name = "client_id")
-    )
+    @CollectionTable(name = "clients_contacts", joinColumns = @JoinColumn(name = "client_id"))
     @MapKeyColumn(name = "type")
     @Column(name = "value")
     private Map<String, String> contacts;
---
+++ src/test/java/br/gabriel/jpaspecialist/ddl/DDLTest.java
@@ -0,0 +1,11 @@
+package br.gabriel.jpaspecialist.ddl;
+
+import br.gabriel.jpaspecialist.BaseEntityManager;
+import org.junit.Test;
+
+public class DDLTest extends BaseEntityManager {
+    @Test
+    public void shouldTestDDL() {
+        // Just to execute it
+    }
+}
