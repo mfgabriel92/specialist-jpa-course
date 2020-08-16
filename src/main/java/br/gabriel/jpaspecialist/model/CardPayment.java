c137dd56accc2832e1aa69f5ce5da354b426e641
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-08-16 11:26:40 -0300

    6.20. Back to the single table strategy mapping

--- src/main/java/br/gabriel/jpaspecialist/model/CardPayment.java
+++ src/main/java/br/gabriel/jpaspecialist/model/CardPayment.java
@@ -5,11 +5,9 @@ import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
 @DiscriminatorValue("card")
-@Table(name = "card_payments")
 @Getter
 @Setter
 public class CardPayment extends Payment {
--- src/main/java/br/gabriel/jpaspecialist/model/SlipPayment.java
+++ src/main/java/br/gabriel/jpaspecialist/model/SlipPayment.java
@@ -5,11 +5,9 @@ import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
 @DiscriminatorValue("slip")
-@Table(name = "slip_payments")
 @Getter
 @Setter
 public class SlipPayment extends Payment {
