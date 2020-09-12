f540e104a1d8c25384740bedf37a02a0eece334a
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-09-12 14:51:57 -0300

    9.27. Grouping records with group by

---
+++ src/test/java/br/gabriel/jpaspecialist/jpql/GroupByTest.java
@@ -0,0 +1,34 @@
+package br.gabriel.jpaspecialist.jpql;
+
+import br.gabriel.jpaspecialist.BaseEntityManager;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class GroupByTest extends BaseEntityManager {
+    @Test
+    public void shouldGroupProductsByCategory() {
+        String jpql = "SELECT c.name, COUNT(p.id) FROM Category c JOIN c.products p GROUP BY c.id";
+    
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    
+        list.forEach(i -> System.out.printf("%s: %s\n", i[0], i[1]));
+    }
+    
+    @Test
+    public void shouldGroupSalesPerMonth() {
+        String jpql = "SELECT CONCAT(FUNCTION('year', o.createdAt), '/', FUNCTION('monthname', o.createdAt) ), SUM(o.total) " +
+            "FROM Order o " +
+            "GROUP BY FUNCTION('year', o.createdAt), FUNCTION('month', o.createdAt) ";
+        
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+        
+        list.forEach(i -> System.out.printf("%s: %s\n", i[0], i[1]));
+    }
+}
